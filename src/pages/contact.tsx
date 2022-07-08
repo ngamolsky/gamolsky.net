ce3dca6b802ace3cebb22ffd2062a28e1585b7e6
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2022-07-08 13:25:25 -0700

    add contact buttons

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,11 +9,30 @@ const ContactPage = () => {
       currentPage="contact"
     >
       <SEO title="Contact" />
-
-      <a href="resume.pdf" download>
-        Download Resume
-      </a>
-      <a href="resume.pdf">Open Resume</a>
+      <div className="flex flex-col p-4 mt-4 space-y-8">
+        <a
+          href="mailto:nikita@gamolsky.net?subject=Hello From Your Website!"
+          className="w-64 py-4 mx-auto text-2xl text-center rounded-2xl bg-pink"
+          target={"_blank"}
+        >
+          Email Me
+        </a>
+        <a
+          download
+          href="resume.pdf"
+          className="w-64 py-4 mx-auto text-2xl text-center rounded-2xl bg-yellow"
+        >
+          Download Resume
+        </a>
+        <a
+          download
+          href="http://www.linkedin.com/in/ngamolsky"
+          className="w-64 py-4 mx-auto text-2xl text-center rounded-2xl bg-darkerblue"
+          target={"_blank"}
+        >
+          LinkedIn
+        </a>
+      </div>
     </Container>
   );
 };
