27ed4b7fefb226e986026cccaf4e13c9886bc401
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-11-19 18:12:37 -0800

    default to dark mode after all

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,19 +1,14 @@
 import { useState, useEffect } from "react";
 
 export const useTheme: () => [string, (newTheme: string) => void] = () => {
-  const [theme, setTheme] = useState<string>("system");
+  const [theme, setTheme] = useState<string>("dark");
 
   useEffect(() => {
-    setTheme(localStorage.getItem("theme") || "system");
+    setTheme(localStorage.getItem("theme") || "dark");
   }, []);
 
   const handleThemeChange = (newTheme: string) => {
-    if (newTheme === "system") {
-      localStorage.removeItem("theme");
-    } else {
-      localStorage.setItem("theme", newTheme);
-    }
-
+    localStorage.setItem("theme", newTheme);
     setTheme(newTheme);
   };
 
