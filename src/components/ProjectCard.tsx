9330bec41af83ce6ae0917e12d8dda772dc58e7a
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-11-21 09:28:05 -0800

    remove links all together

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -108,42 +108,6 @@ export const ProjectCard = ({
           <div className="grow" />
           <div className="dark:bg-black bg-white dark:text-white text-black p-2 opacity-80 flex flex-col gap-1">
             <div className="text-xs">{description}</div>
-            {/* {lastEditedOn && (
-              <div className="flex text-xs text-slate-500 ">
-                <div className="self-end ">
-                  Last Updated on: {lastEditedOn.toLocaleDateString()}{" "}
-                </div>
-              </div>
-            )} */}
-            {/* <div className="flex space-x-4 text-sm">
-              {notionLink && (
-                <a
-                  className="self-end text-yellow  hover:underline"
-                  href={notionLink}
-                  target="_blank"
-                >
-                  Notion
-                </a>
-              )}
-              {githubLink && (
-                <a
-                  className="self-end text-yellow  hover:underline "
-                  href={githubLink}
-                  target="_blank"
-                >
-                  Github
-                </a>
-              )}
-              {actionLink && (
-                <a
-                  className="self-end text-yellow  hover:underline "
-                  href={actionLink}
-                  target="_blank"
-                >
-                  Check it Out!
-                </a>
-              )}
-            </div> */}
           </div>
         </div>
       </motion.div>
