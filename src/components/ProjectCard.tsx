0917656167b8b5214940d42cdf6ef2b02407ff07
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-11-20 21:00:04 -0800

    restore links but remove last editon on

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -106,14 +106,14 @@ export const ProjectCard = ({
           <div className="grow" />
           <div className="dark:bg-black bg-white dark:text-white text-black p-2 opacity-80 flex flex-col gap-1">
             <div className="text-xs">{description}</div>
-            {lastEditedOn && (
+            {/* {lastEditedOn && (
               <div className="flex text-xs text-slate-500 ">
                 <div className="self-end ">
                   Last Updated on: {lastEditedOn.toLocaleDateString()}{" "}
                 </div>
               </div>
-            )}
-            {/* <div className="flex space-x-4 text-sm">
+            )} */}
+            <div className="flex space-x-4 text-sm">
               {notionLink && (
                 <a
                   className="self-end text-yellow  hover:underline"
@@ -141,7 +141,7 @@ export const ProjectCard = ({
                   Check it Out!
                 </a>
               )}
-            </div> */}
+            </div>
           </div>
         </div>
       </motion.div>
