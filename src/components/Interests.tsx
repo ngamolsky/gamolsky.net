982ed4b5d73b4232f11feae5713af2abacc84a14
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2023-11-21 10:19:25 -0800

    try static project

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -1,11 +1,45 @@
 import * as React from "react";
-import { useProjects, Tag } from "../hooks/useProjects";
+import { useProjects, Tag, Project } from "../hooks/useProjects";
 import { ProjectCard } from "./ProjectCard";
 import { TAG_TO_COLOR_MAP } from "../utils/colors";
 
 export const Interests = () => {
   const projects = useProjects();
   const [selectedTag, setSelectedTag] = React.useState<Tag>();
+
+  const onlyProject: Project = {
+    title: "Evvy",
+    link: "",
+    description:
+      "Know what’s up down there. Get innovative and integrative vaginal healthcare, powered by our state-of-the-art vaginal microbiome test.",
+    imageData: {
+      childImageSharp: {
+        gatsbyImageData: {
+          layout: "constrained",
+          placeholder: {
+            fallback:
+              "data:image/webp;base64,UklGRnAAAABXRUJQVlA4IGQAAADQAwCdASoUABQAPtFapk0oJSOiMBgIAQAaCWMAsOwQ7+vhIWOo2EAA/ucafumqtaO/UGQZ2pGtJ/8O83WwcUYRS6omWezs/Nx3bE6+ijQKuCD6cW+FhXUPxK/CIaNtPKwwAAAA",
+          },
+          images: {
+            fallback: {
+              src: "/static/4ae77107c8e05ad75f6a00649be311d4/318ad/63cf82da867ebf6821035cf7_PDP_Care_Image.webp",
+              srcSet:
+                "/static/4ae77107c8e05ad75f6a00649be311d4/41704/63cf82da867ebf6821035cf7_PDP_Care_Image.webp 315w,\n/static/4ae77107c8e05ad75f6a00649be311d4/27494/63cf82da867ebf6821035cf7_PDP_Care_Image.webp 630w,\n/static/4ae77107c8e05ad75f6a00649be311d4/318ad/63cf82da867ebf6821035cf7_PDP_Care_Image.webp 1260w",
+              sizes: "(min-width: 1260px) 1260px, 100vw",
+            },
+            sources: [],
+          },
+          width: 1260,
+          height: 1243.9999999999998,
+        },
+      },
+    },
+    notionLink: "https://www.notion.so/Evvy-fe3ef79fabbe4bb28a36cb272fc71c24",
+    actionLink: "https://evvy.com",
+    lastEditedOn: new Date("2023-11-16T07:07:00.000Z"),
+    status: "In Progress",
+    tags: [Tag.Health, Tag.Work],
+  };
   return (
     <section
       id="interests"
@@ -58,8 +92,8 @@ export const Interests = () => {
         {projects.map((project, index) => {
           return (
             <ProjectCard
-              key={project.title}
-              project={project}
+              key={index}
+              project={onlyProject}
               index={index}
               selectedTag={
                 selectedTag && project.tags.includes(selectedTag)
