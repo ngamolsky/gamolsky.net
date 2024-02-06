f85e1382ca35dc241a7cd813fbd935050aa41a46
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2024-02-06 12:23:00 -0800

    fix settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import { DarkModeCheckbox } from "./DarkmodeCheckbox";
+import { setDarkModeClassFromLocalStorageOrSetting } from "../utils/colors";
 import { useTheme } from "../hooks/useTheme";
 import { useBreakpoint } from "gatsby-plugin-breakpoints";
 
 export const Settings = () => {
   const [theme, setTheme] = useTheme();
   const breakpoints = useBreakpoint();
+
+  React.useEffect(() => {
+    setDarkModeClassFromLocalStorageOrSetting();
+  }, [theme]);
 
   return (
     <section
